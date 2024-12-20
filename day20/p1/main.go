af4df77fb685bc12666103ba049e5b99a69aad00
Michael Bordner <mbordner@gmail.com>
2024-12-20 03:24:09 -0500

    day 20

--- day20/p1/main.go
+++ day20/p1/main.go
@@ -6,16 +6,12 @@ import (
 	"github.com/mbordner/aoc2024/common/file"
 	"github.com/mbordner/aoc2024/common/graph"
 	"github.com/mbordner/aoc2024/common/graph/djikstra"
+	"sort"
 )
 
-type Cheat struct {
-	p1        common.Pos
-	p2        common.Pos
-	reduction int
-}
-
+// 5520 too high
 func main() {
-	s, e, g := getData("../test.txt")
+	s, e, g := getData("../data.txt")
 
 	grid := getGrid(g)
 
@@ -28,7 +24,7 @@ func main() {
 	startPosition := s.GetID().(common.Pos)
 
 	prev := startPosition
-	for i, n := range path[0 : len(path)-1] {
+	for i, n := range path {
 		p := n.GetID().(common.Pos)
 		pathPositions[i] = p
 		pathPosContainer[p] = true
@@ -46,7 +42,9 @@ func main() {
 				r = rune('^')
 			}
 		}
-		grid[p.Y][p.X] = byte(r)
+		if n != e {
+			grid[p.Y][p.X] = byte(r)
+		}
 		prev = p
 	}
 
@@ -57,6 +55,82 @@ func main() {
 	fmt.Println(s, e, g.Len())
 	fmt.Println("shortest path cost:", cost)
 
+	cheats := calculateCheats(startPosition, pathPositions, pathPosContainer)
+
+	saves := make([]int, 0, len(cheats))
+	for s := range cheats {
+		saves = append(saves, s)
+	}
+	sort.Ints(saves)
+
+	count := 0
+	for _, s := range saves {
+		if s <= 100 {
+			count += len(cheats[s])
+		}
+		fmt.Printf("There are %d cheats that save %d picoseconds\n", len(cheats[s]), s)
+	}
+
+	fmt.Println("number of cheats that would save you at least 100 picoseconds:", count)
+
+}
+
+type Cheat struct {
+	P1 common.Pos
+	P2 common.Pos
+}
+
+type Cheats map[int][]Cheat
+
+func (cs Cheats) Add(c Cheat, reduction int) {
+	if _, e := cs[reduction]; e {
+		cs[reduction] = append(cs[reduction], c)
+	} else {
+		cs[reduction] = []Cheat{c}
+	}
+}
+
+func calculateCheats(sp common.Pos, path common.Positions, pc common.PosContainer) Cheats {
+	pathWithStart := append(common.Positions{sp}, path...)
+
+	cheats := make(Cheats)
+
+	step := make(map[common.Pos]int)
+	for i, p := range pathWithStart {
+		step[p] = i
+	}
+
+	for _, p := range pathWithStart {
+		possibleCheats := make([]Cheat, 0, 16)
+		for y1 := p.Y - 1; y1 <= p.Y+1; y1++ {
+			for x1 := p.X - 1; x1 <= p.X+1; x1++ {
+				if (x1 == p.X && y1 != p.Y) || (y1 == p.Y && x1 != p.X) {
+					for y2 := y1 - 1; y2 <= y1+1; y2++ {
+						for x2 := x1 - 1; x2 <= x1+1; x2++ {
+							if (y2 == y1 && x2 != x1) || (x2 == x1 && y2 != y1) {
+								p1 := common.Pos{Y: y1, X: x1}
+								p2 := common.Pos{Y: y2, X: x2}
+								if !pc.Has(p1) && pc.Has(p2) {
+									if step[p2] > step[p] {
+										possibleCheats = append(possibleCheats, Cheat{P1: p1, P2: p2})
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		for _, pc := range possibleCheats {
+			reduction := step[pc.P2] - step[p] - 2
+			if reduction > 0 {
+				cheats.Add(pc, reduction)
+			}
+		}
+	}
+
+	return cheats
 }
 
 func getGrid(g *graph.Graph) common.Grid {
