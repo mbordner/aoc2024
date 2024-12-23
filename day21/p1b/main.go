b2fb5da7404083a5068ec8763326f527faf724b4
Michael Bordner <mbordner@gmail.com>
2024-12-23 01:31:56 -0500

    day 23

--- day21/p1b/main.go
+++ day21/p1b/main.go
@@ -1,5 +1,241 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mbordner/aoc2024/common"
+	"github.com/mbordner/aoc2024/common/file"
+	"github.com/mbordner/aoc2024/common/graph"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	reDigits = regexp.MustCompile(`^0*(\d+)`)
+)
+
+const (
+	D = "v"
+	L = "<"
+	R = ">"
+	U = "^"
+)
+
+// 177830 too high
+// 183830 too high
+
+// 182112 too high
+// 182810 not right
 func main() {
-	
+	numPadGraph := getNumPadGraph()
+	dirPadGraph := getDirPadGraph()
+	fmt.Println(numPadGraph.Len(), dirPadGraph.Len())
+
+	lines, _ := file.GetLines("../test.txt")
+
+	sum := 0
+
+	for _, line := range lines {
+		getSequencesForSequence(numPadGraph, line)
+	}
+
+	fmt.Printf("\n\nAnswer: %d\n", sum)
+
+}
+
+func pretty(s string) string {
+	return strings.Join(strings.Split(s, "A"), "A ")
+}
+
+func getNumVal(s string) int {
+	if reDigits.MatchString(s) {
+		match := reDigits.FindStringSubmatch(s)
+		val, _ := strconv.Atoi(match[1])
+		return val
+	}
+	return 0
+}
+
+type SeqMem map[string][]string
+
+func (sm SeqMem) Has(seq string) bool {
+	if _, e := sm[seq]; e {
+		return true
+	}
+	return false
+}
+
+type GraphSeqMem map[*graph.Graph]SeqMem
+
+var (
+	memSeq = make(GraphSeqMem)
+)
+
+func (gsm GraphSeqMem) Has(g *graph.Graph, seq string) bool {
+	if _, e := gsm[g]; !e {
+		gsm[g] = make(SeqMem)
+		return false
+	} else if gsm[g].Has(seq) {
+		return true
+	}
+	return false
+}
+
+func getPathsForSequence(g *graph.Graph, seq string) []string {
+	if memSeq.Has(g, seq) {
+		return memSeq[g][seq]
+	}
+
+	var sequences []string
+
+	for i := 0; i < len(seq)-1; i++ {
+
+		queue := make(common.Queue[graph.NodeValue], 0, g.Len())
+		visited := make(graph.VisitedNodes, 0, g.Len())
+
+		start := g.GetNode(seq[i : i+1])
+		goal := g.GetNode(seq[i+1 : i+2])
+
+		queue.Enqueue(graph.NodeValue{Node: start, Value: 0})
+
+		for !queue.Empty() {
+			cur := *(queue.Dequeue())
+			if cur.Node == goal {
+				solution := []string{cur.EdgeTaken.GetProperty("dir").(string)}
+				for c := cur.PreviousNodeValue; c != nil && c.EdgeTaken != nil; c = c.PreviousNodeValue {
+					solution = append([]string{c.EdgeTaken.GetProperty("dir").(string)}, solution...)
+				}
+				sequence := strings.Join(solution, "")
+				if len(sequences) == 0 || len(sequence) == len(sequences[0]) {
+					sequences = append(sequences, sequence)
+				} else {
+					break
+				}
+			} else {
+				edges := cur.Node.GetTraversableEdges()
+				for _, edge := range edges {
+					dest := edge.GetDestination()
+					if !visited.Contains(dest) {
+						visited = append(visited, dest)
+						queue.Enqueue(graph.NodeValue{Node: dest, EdgeTaken: edge, PreviousNodeValue: &cur, PreviousNode: cur.Node, Value: 1})
+					}
+				}
+			}
+
+		}
+	}
+
+	return sequences
+}
+
+func combineSequences(previousSequences, newSequences []string) []string {
+	product := common.CartesianProduct([][]string{previousSequences, newSequences})
+	sequences := make([]string, len(product))
+	for i, p := range product {
+		sequences[i] = strings.Join(p, "")
+	}
+	return sequences
+}
+
+func getSequencesForSequence(g *graph.Graph, seq string) []string {
+	var sequences []string
+
+	seqFromA := "A" + seq
+
+	for i := 0; i < len(seqFromA)-1; i++ {
+		paths := getPathsForSequence(g, seqFromA[i:i+2])
+		if len(sequences) == 0 {
+			sequences = paths
+		} else {
+			sequences = combineSequences(sequences, paths)
+		}
+	}
+
+	return sequences
+}
+
+func getNumPadGraph() *graph.Graph {
+	g := graph.NewGraph()
+
+	bA := g.CreateNode("A")
+	b0 := g.CreateNode("0")
+	b1 := g.CreateNode("1")
+	b2 := g.CreateNode("2")
+	b3 := g.CreateNode("3")
+	b4 := g.CreateNode("4")
+	b5 := g.CreateNode("5")
+	b6 := g.CreateNode("6")
+	b7 := g.CreateNode("7")
+	b8 := g.CreateNode("8")
+	b9 := g.CreateNode("9")
+
+	bA.AddEdge(b3, 1).AddProperty("dir", U)
+	bA.AddEdge(b0, 1).AddProperty("dir", L)
+
+	b0.AddEdge(bA, 1).AddProperty("dir", R)
+	b0.AddEdge(b2, 1).AddProperty("dir", U)
+
+	b1.AddEdge(b2, 1).AddProperty("dir", R)
+	b1.AddEdge(b4, 1).AddProperty("dir", U)
+
+	b2.AddEdge(b3, 1).AddProperty("dir", R)
+	b2.AddEdge(b0, 1).AddProperty("dir", D)
+	b2.AddEdge(b5, 1).AddProperty("dir", U)
+	b2.AddEdge(b1, 1).AddProperty("dir", L)
+
+	b3.AddEdge(bA, 1).AddProperty("dir", D)
+	b3.AddEdge(b6, 1).AddProperty("dir", U)
+	b3.AddEdge(b2, 1).AddProperty("dir", L)
+
+	b4.AddEdge(b5, 1).AddProperty("dir", R)
+	b4.AddEdge(b1, 1).AddProperty("dir", D)
+	b4.AddEdge(b7, 1).AddProperty("dir", U)
+
+	b5.AddEdge(b6, 1).AddProperty("dir", R)
+	b5.AddEdge(b2, 1).AddProperty("dir", D)
+	b5.AddEdge(b8, 1).AddProperty("dir", U)
+	b5.AddEdge(b4, 1).AddProperty("dir", L)
+
+	b6.AddEdge(b9, 1).AddProperty("dir", U)
+	b6.AddEdge(b5, 1).AddProperty("dir", L)
+	b6.AddEdge(b3, 1).AddProperty("dir", D)
+
+	b7.AddEdge(b8, 1).AddProperty("dir", R)
+	b7.AddEdge(b4, 1).AddProperty("dir", D)
+
+	b8.AddEdge(b9, 1).AddProperty("dir", R)
+	b8.AddEdge(b5, 1).AddProperty("dir", D)
+	b8.AddEdge(b7, 1).AddProperty("dir", L)
+
+	b9.AddEdge(b6, 1).AddProperty("dir", D)
+	b9.AddEdge(b8, 1).AddProperty("dir", L)
+
+	return g
+}
+
+func getDirPadGraph() *graph.Graph {
+	g := graph.NewGraph()
+
+	bA := g.CreateNode("A")
+	bR := g.CreateNode(R)
+	bU := g.CreateNode(U)
+	bD := g.CreateNode(D)
+	bL := g.CreateNode(L)
+
+	bA.AddEdge(bR, 1).AddProperty("dir", D)
+	bA.AddEdge(bU, 1).AddProperty("dir", L)
+
+	bR.AddEdge(bA, 1).AddProperty("dir", U)
+	bR.AddEdge(bD, 1).AddProperty("dir", L)
+
+	bU.AddEdge(bA, 1).AddProperty("dir", R)
+	bU.AddEdge(bD, 1).AddProperty("dir", D)
+
+	bD.AddEdge(bU, 1).AddProperty("dir", U)
+	bD.AddEdge(bR, 1).AddProperty("dir", R)
+	bD.AddEdge(bL, 1).AddProperty("dir", L)
+
+	bL.AddEdge(bD, 1).AddProperty("dir", R)
+
+	return g
 }
--- day23/p1/main.go
+++ day23/p1/main.go
@@ -1,5 +1,95 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mbordner/aoc2024/common/file"
+	"github.com/mbordner/aoc2024/common/graph"
+	"sort"
+	"strings"
+)
 
+type FNT struct {
+	from *graph.Node
+	node *graph.Node
+	to   *graph.Node
+}
+
+func (fnt FNT) String() string {
+	from := fnt.from.GetID().(string)
+	node := fnt.node.GetID().(string)
+	to := fnt.to.GetID().(string)
+
+	//if strings.Compare(to, from) < 0 {
+	//	from, to = to, from
+	//}
+
+	return fmt.Sprintf("%s,%s,%s", from, node, to)
+}
+
+func main() {
+	g := getGraph("../data.txt")
+
+	nodes := g.GetNodes()
+
+	connections := make(map[FNT]bool)
+
+	for _, fromNode := range nodes {
+		for _, fromEdge := range fromNode.GetEdges() {
+			node := fromEdge.GetDestination()
+			for _, toEdge := range node.GetEdges() {
+				toNode := toEdge.GetDestination()
+				if toNode != fromNode && toNode != node {
+					for _, checkEdge := range toNode.GetEdges() {
+						if checkEdge.GetDestination() == fromNode {
+							connections[FNT{fromNode, node, toNode}] = true
+						}
+					}
+				}
+			}
+		}
+	}
+
+	connectionSets := make(map[string]bool)
+	for c := range connections {
+		cs := strings.Split(c.String(), ",")
+		sort.Strings(cs)
+		connectionSets[strings.Join(cs, ",")] = true
+	}
+
+	count := 0
+nextSet:
+	for cs := range connectionSets {
+		tokens := strings.Split(cs, ",")
+		for _, token := range tokens {
+			if token[0] == 't' {
+				count++
+				continue nextSet
+			}
+		}
+	}
+
+	fmt.Println(count)
+
+}
+
+func getGraph(f string) *graph.Graph {
+	g := graph.NewGraph()
+
+	lines, _ := file.GetLines(f)
+	for _, line := range lines {
+		tokens := strings.Split(line, "-")
+
+		var n, o *graph.Node
+		if n = g.GetNode(tokens[0]); n == nil {
+			n = g.CreateNode(tokens[0])
+		}
+		if o = g.GetNode(tokens[1]); o == nil {
+			o = g.CreateNode(tokens[1])
+		}
+
+		n.AddEdge(o, 1)
+		o.AddEdge(n, 1)
+	}
+
+	return g
 }
