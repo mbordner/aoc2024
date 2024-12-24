1c469214743392f82377db7c578fc29b41f8f929
Michael Bordner <mbordner@gmail.com>
2024-12-23 22:51:20 -0500

    day 21

--- day21/p2/main.go
+++ day21/p2/main.go
@@ -1,5 +1,333 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mbordner/aoc2024/common"
+	"github.com/mbordner/aoc2024/common/file"
+	"github.com/mbordner/aoc2024/common/graph"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	reDigits = regexp.MustCompile(`^0*(\d+)`)
+)
+
+const (
+	D = "v"
+	L = "<"
+	R = ">"
+	U = "^"
+)
+
+var (
+	numPadGraph = getNumPadGraph()
+	dirPadGraph = getDirPadGraph()
+)
+
+// 5501853103614  too low
 func main() {
 
+	lines, _ := file.GetLines("../data.txt")
+
+	sum := 0
+
+	//checkValues := []string{
+	//	`<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A`,
+	//	`<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A`,
+	//	`<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A`,
+	//	`<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A`,
+	//	`<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A`,
+	//}
+
+	for i, line := range lines {
+		fmt.Printf("processing line %d: %s\n", i+1, line)
+		length := getPathSeqMinLength(line, 25)
+		fmt.Println(length)
+		sum += length * getNumVal(line)
+	}
+
+	fmt.Printf("\n\nAnswer: %d\n", sum)
+
+}
+
+type DepthSequence struct {
+	s string
+	d int
+}
+
+var (
+	memDepthSequence          = make(map[DepthSequence][]string)
+	memDepthSequenceMinLength = make(map[DepthSequence]int)
+)
+
+func getPathSeqMinLength(sequence string, depth int) int {
+	numPadDirections := getSequencesForSequence(numPadGraph, sequence)
+	minLength := math.MaxInt64
+	for _, seq := range numPadDirections {
+		l := getPathSeqLength(seq, depth)
+		if l < minLength {
+			minLength = l
+		}
+	}
+	return minLength
+}
+
+func getPathSeqLength(sequence string, depth int) int {
+	if l, e := memDepthSequenceMinLength[DepthSequence{sequence, depth}]; e {
+		return l
+	}
+
+	dirSequence := "A" + sequence
+	length := 0
+
+	if depth == 1 {
+		for i := 0; i < len(dirSequence)-1; i++ {
+			sequences := getPathsForSequence(dirPadGraph, dirSequence[i:i+2])
+			length += len(sequences[0])
+		}
+		return length
+	}
+
+	for i := 0; i < len(dirSequence)-1; i++ {
+		sequences := getPathsForSequence(dirPadGraph, dirSequence[i:i+2])
+		seqMinLength := math.MaxInt64
+		for _, seq := range sequences {
+			seqLength := getPathSeqLength(seq, depth-1)
+			if seqLength < seqMinLength {
+				seqMinLength = seqLength
+			}
+		}
+		length += seqMinLength
+	}
+
+	memDepthSequenceMinLength[DepthSequence{sequence, depth}] = length
+	return length
+}
+
+func getPathSeq(sequence string, depth int) []string {
+	if s, e := memDepthSequence[DepthSequence{sequence, depth}]; e {
+		return s
+	}
+	if depth == 0 {
+		return getPathsForSequence(numPadGraph, sequence)
+	}
+	prevSequences := getPathSeq(sequence, depth-1)
+	var nextSequences []string
+	for _, prevSeq := range prevSequences {
+		var sequences []string
+		seq := "A" + prevSeq
+		for i := 0; i < len(seq)-1; i++ {
+			sequences = combineSequences(sequences, getPathsForSequence(dirPadGraph, seq[i:i+2]))
+		}
+		if len(nextSequences) == 0 || len(sequences[0]) < len(nextSequences[0]) {
+			nextSequences = sequences
+		} else if len(sequences[0]) == len(nextSequences[0]) {
+			nextSequences = append(nextSequences, sequences...)
+		} else {
+			// we're leaving off some possibilities here, can we really do this?  not sure
+		}
+	}
+	memDepthSequence[DepthSequence{sequence, depth}] = nextSequences
+	return nextSequences
+}
+
+func getNumVal(s string) int {
+	if reDigits.MatchString(s) {
+		match := reDigits.FindStringSubmatch(s)
+		val, _ := strconv.Atoi(match[1])
+		return val
+	}
+	return 0
+}
+
+type SeqMem map[string][]string
+
+func (sm SeqMem) Has(seq string) bool {
+	if _, e := sm[seq]; e {
+		return true
+	}
+	return false
+}
+
+type GraphSeqMem map[*graph.Graph]SeqMem
+
+var (
+	memSeq = make(GraphSeqMem)
+)
+
+func (gsm GraphSeqMem) Has(g *graph.Graph, seq string) bool {
+	if _, e := gsm[g]; !e {
+		gsm[g] = make(SeqMem)
+		return false
+	} else if gsm[g].Has(seq) {
+		return true
+	}
+	return false
+}
+
+func getPathsForSequence(g *graph.Graph, seq string) []string {
+	if memSeq.Has(g, seq) {
+		return memSeq[g][seq]
+	}
+
+	var sequences []string
+
+	type state struct {
+		n   *graph.Node
+		seq string
+	}
+
+	for i := 0; i < len(seq)-1; i++ {
+
+		queue := make(common.Queue[state], 0, g.Len())
+
+		start := g.GetNode(seq[i : i+1])
+		goal := g.GetNode(seq[i+1 : i+2])
+
+		queue.Enqueue(state{n: start, seq: ""})
+
+		for !queue.Empty() {
+			cur := *(queue.Dequeue())
+			if cur.n == goal {
+				seqWithA := cur.seq + "A"
+				if len(sequences) == 0 || len(seqWithA) == len(sequences[0]) {
+					sequences = append(sequences, seqWithA)
+				} else if len(seqWithA) < len(sequences[0]) {
+					sequences = []string{seqWithA}
+				} else {
+					break
+				}
+			} else {
+				edges := cur.n.GetTraversableEdges()
+				for _, edge := range edges {
+					dest := edge.GetDestination()
+					destSeq := cur.seq + edge.GetProperty("dir").(string)
+					if len(sequences) == 0 || len(destSeq) <= len(sequences[0]) {
+						queue.Enqueue(state{n: dest, seq: destSeq})
+					}
+				}
+			}
+
+		}
+	}
+
+	return sequences
+}
+
+func combineSequences(previousSequences, newSequences []string) []string {
+	if len(previousSequences) == 0 {
+		return newSequences
+	}
+	product := common.CartesianProduct([][]string{previousSequences, newSequences})
+	sequences := make([]string, len(product))
+	for i, p := range product {
+		sequences[i] = strings.Join(p, "")
+	}
+	return sequences
+}
+
+func getSequencesForSequence(g *graph.Graph, seq string) []string {
+	var sequences []string
+
+	seqFromA := "A" + seq
+
+	for i := 0; i < len(seqFromA)-1; i++ {
+		paths := getPathsForSequence(g, seqFromA[i:i+2])
+		if len(sequences) == 0 {
+			sequences = paths
+		} else {
+			sequences = combineSequences(sequences, paths)
+		}
+	}
+
+	return sequences
+}
+
+func getNumPadGraph() *graph.Graph {
+	g := graph.NewGraph()
+
+	bA := g.CreateNode("A")
+	b0 := g.CreateNode("0")
+	b1 := g.CreateNode("1")
+	b2 := g.CreateNode("2")
+	b3 := g.CreateNode("3")
+	b4 := g.CreateNode("4")
+	b5 := g.CreateNode("5")
+	b6 := g.CreateNode("6")
+	b7 := g.CreateNode("7")
+	b8 := g.CreateNode("8")
+	b9 := g.CreateNode("9")
+
+	bA.AddEdge(b3, 1).AddProperty("dir", U)
+	bA.AddEdge(b0, 1).AddProperty("dir", L)
+
+	b0.AddEdge(bA, 1).AddProperty("dir", R)
+	b0.AddEdge(b2, 1).AddProperty("dir", U)
+
+	b1.AddEdge(b2, 1).AddProperty("dir", R)
+	b1.AddEdge(b4, 1).AddProperty("dir", U)
+
+	b2.AddEdge(b3, 1).AddProperty("dir", R)
+	b2.AddEdge(b0, 1).AddProperty("dir", D)
+	b2.AddEdge(b5, 1).AddProperty("dir", U)
+	b2.AddEdge(b1, 1).AddProperty("dir", L)
+
+	b3.AddEdge(bA, 1).AddProperty("dir", D)
+	b3.AddEdge(b6, 1).AddProperty("dir", U)
+	b3.AddEdge(b2, 1).AddProperty("dir", L)
+
+	b4.AddEdge(b5, 1).AddProperty("dir", R)
+	b4.AddEdge(b1, 1).AddProperty("dir", D)
+	b4.AddEdge(b7, 1).AddProperty("dir", U)
+
+	b5.AddEdge(b6, 1).AddProperty("dir", R)
+	b5.AddEdge(b2, 1).AddProperty("dir", D)
+	b5.AddEdge(b8, 1).AddProperty("dir", U)
+	b5.AddEdge(b4, 1).AddProperty("dir", L)
+
+	b6.AddEdge(b9, 1).AddProperty("dir", U)
+	b6.AddEdge(b5, 1).AddProperty("dir", L)
+	b6.AddEdge(b3, 1).AddProperty("dir", D)
+
+	b7.AddEdge(b8, 1).AddProperty("dir", R)
+	b7.AddEdge(b4, 1).AddProperty("dir", D)
+
+	b8.AddEdge(b9, 1).AddProperty("dir", R)
+	b8.AddEdge(b5, 1).AddProperty("dir", D)
+	b8.AddEdge(b7, 1).AddProperty("dir", L)
+
+	b9.AddEdge(b6, 1).AddProperty("dir", D)
+	b9.AddEdge(b8, 1).AddProperty("dir", L)
+
+	return g
+}
+
+func getDirPadGraph() *graph.Graph {
+	g := graph.NewGraph()
+
+	bA := g.CreateNode("A")
+	bR := g.CreateNode(R)
+	bU := g.CreateNode(U)
+	bD := g.CreateNode(D)
+	bL := g.CreateNode(L)
+
+	bA.AddEdge(bR, 1).AddProperty("dir", D)
+	bA.AddEdge(bU, 1).AddProperty("dir", L)
+
+	bR.AddEdge(bA, 1).AddProperty("dir", U)
+	bR.AddEdge(bD, 1).AddProperty("dir", L)
+
+	bU.AddEdge(bA, 1).AddProperty("dir", R)
+	bU.AddEdge(bD, 1).AddProperty("dir", D)
+
+	bD.AddEdge(bU, 1).AddProperty("dir", U)
+	bD.AddEdge(bR, 1).AddProperty("dir", R)
+	bD.AddEdge(bL, 1).AddProperty("dir", L)
+
+	bL.AddEdge(bD, 1).AddProperty("dir", R)
+
+	return g
 }
